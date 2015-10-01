da1865582410dffc16dcdb122c7beb305cf96625
sergueik <kouzmine_serguei@yahoo.com>
2015-10-01 19:59:41 -0400

    intermediate revision, unfinished

--- lib/specinfra/command/windows/base/service.rb
+++ lib/specinfra/command/windows/base/service.rb
@@ -27,5 +27,31 @@ class Specinfra::Command::Windows::Base::Service < Specinfra::Command::Windows::
         exec "(FindService -name '#{service}').State -eq 'Running'"
       end
     end
+    def check_service_is_running_under_localsystem
+      $selector = "| Where-Object { -not (($_.StartName -match 'NT AUTHORITY') -or ( $_.StartName -match 'NT SERVICE') -or ($_.StartName -match 'NetworkService' ) -or ($_.StartName -match 'LocalSystem' ))"
+    end
+
+    def check_has_property(service,property,value=nil)
+      Backend::PowerShell::Command.new do
+        using 'find_service.ps1'
+        if value
+          exec "((FindService -name '#{service}').'#{property}' -ne $null)"
+        else
+          exec "((FindService -name '#{service}').'#{property}' -match '#{value}')"
+          # exec "((FindService -name '#{service}')| ConvertTo-Json) -replace '\\r?\\n', ' ' "
+        end 
+      end
+    end
+    
+    def get_property(service, property = nil)
+      Backend::PowerShell::Command.new do
+        using 'find_service.ps1'
+        if property 
+          (exec "(FindService -name '#{service}').#{property}").stdout
+        else
+          (exec "((FindService -name '#{service}')| ConvertTo-Json) -replace '\\r?\\n', ' '").stdout
+        end 
+      end
+    end
   end
 end
