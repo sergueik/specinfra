d031e154e097c3443a7e17564c25fc118035c442
sergueik <kouzmine_serguei@yahoo.com>
2016-01-29 18:47:58 -0500

    intermediate revision - updating signatures

--- lib/specinfra/command/windows/base/yaml_file.rb
+++ lib/specinfra/command/windows/base/yaml_file.rb
@@ -1,21 +1,21 @@
 class Specinfra::Command::Windows::Base::YamlFile< Specinfra::Command::Windows::Base
   class << self
-    def check_run_has_resource(puppet_run_log, resource_name, resource_type)
-      cmd = generate_command puppet_run_log, resource_name, resource_type, 'FindResource'
+    def check_has_resource(name, resource_name, resource_type)
+      cmd = generate_command name, resource_name, resource_type, 'FindResource'
       Backend::PowerShell::Command.new do
         using 'yaml_serializer2.ps1'
         exec cmd
       end
     end
-    def check_run_has_message(puppet_run_log, text, source)
-      cmd = generate_command puppet_run_log, text, source, 'FindMessage'
+    def check_has_message(name, message)
+      cmd = generate_command name, message, 'FindMessage'
       Backend::PowerShell::Command.new do
         using 'yaml_serializer2.ps1'
         exec cmd
       end
     end
-    def check_run_has_resource_message(puppet_run_log, resource_name, resource_type, text)
-      cmd = generate_command puppet_run_log, resource_name, resource_type, text, 'FindResourceEventMessage'
+    def check_has_resource_message(name, resource_name, resource_type, message)
+      cmd = generate_command name, resource_name, resource_type, message, 'FindResourceEventMessage'
       Backend::PowerShell::Command.new do
         using 'yaml_serializer2.ps1'
         exec cmd
@@ -23,9 +23,9 @@ class Specinfra::Command::Windows::Base::YamlFile< Specinfra::Command::Windows::
     end
     private
     def generate_command item, value, attribute
-     # 	"FindResourceEventMessage '#{puppet_run_log}' -name '#{resource_name}' -type '#{resource_type}' -text '#{message_text}'"
-     # "FindResource -log '#{puppet_run_log}' -name '#{resource_name}' -type '#{resource_type}'"
-     # "FindMessage -log '#{puppet_run_log}' -text '#{text}'" [-source '#{source}']
+     # 	"FindResourceEventMessage '#{name}' -name '#{resource_name}' -type '#{resource_type}' -message '#{message_message}'"
+     # "FindResource -log '#{name}' -name '#{resource_name}' -type '#{resource_type}'"
+     # "FindMessage -log '#{name}' -message '#{message}'" [-source '#{source}']
     end
   end
 end
--- spec/command/windows/yaml_file_spec.rb
+++ spec/command/windows/yaml_file_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 RSpec.describe Specinfra::Command::Windows::Base::YamlFile  do 
-  let(:puppet_run_log) { 'last_run_report.yaml' }
+  let(:puppet_log) { 'last_report.yaml' }
   let(:resource_name) {'dummy'} 
   let(:resource_type) {'Exec'} 
   let(:message) {'executed successfully'}
   let(:source) {'executor'}
 
   describe 'methods' do
-    subject  { should respond_to(:check_is_in_app_pool).with(2).arguments  }
+    subject  { should respond_to(:check_has_message).with(2).arguments  }
   end
   def stub_exec (command)
     expect(described_class).to receive(:exec).with(command)
@@ -19,30 +19,28 @@ RSpec.describe Specinfra::Command::Windows::Base::YamlFile  do
         cmd = "..."
         # exec not called directly
         stub_exec cmd
-        described_class.check_is_in_app_pool name, app_pool
+        described_class.check_has_message puppet_log, message, source
       }
     end
 
-  describe 'check_run_has_resource' do
+  describe 'check_has_resource' do
     it do
-      expect(described_class).to receive(:generate_command).with( puppet_run_log, resource_name, resource_type,'FindResource')
-      described_class.check_run_has_resource puppet_run_log, resource_name, resource_type
+      expect(described_class).to receive(:generate_command).with( puppet_log, resource_name, resource_type,'FindResource')
+      described_class.check_has_resource puppet_log, resource_name, resource_type
     end
   end
 
-  describe 'check_run_has_message' do
+  describe 'check_has_message' do
     it do
-      expect(described_class).to receive(:generate_command).with( puppet_run_log, message, source,'FindMessage')
-      described_class.check_run_has_message puppet_run_log, message, source
+      expect(described_class).to receive(:generate_command).with( puppet_log, message,'FindMessage')
+      described_class.check_has_message puppet_log, message
     end
   end
 
-  describe 'check_run_has_resource_message' do
+  describe 'check_has_resource_message' do
     it do
-      expect(described_class).to receive(:generate_command).with( puppet_run_log, resource_name, resource_type, message,'FindResourceEventMessage')
-      described_class.check_run_has_resource_message puppet_run_log, resource_name, resource_type, message
+      expect(described_class).to receive(:generate_command).with( puppet_log, resource_name, resource_type, message,'FindResourceEventMessage')
+      described_class.check_has_resource_message puppet_log, resource_name, resource_type, message
     end
   end
-
-
 end
